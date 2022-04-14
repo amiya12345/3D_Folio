1b34980ec616c5a243f208c0be0edf3f9beac659
Ankit Samota <ankitkumarsamota121@gmail.com>
2022-04-14 23:37:12 +0530

    :hammer: Fixed BrowserRouter issue

--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Link} from "react-router-dom";
-import Project1 from './components/pages/Project1';
 
 ReactDOM.render(
   <React.StrictMode>
- <App />
-    <BrowserRouter path="/project1">
-    
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
