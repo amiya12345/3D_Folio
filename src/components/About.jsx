74b365ef2bd70866ff686b55f9b95dd3fc4545f6
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2022-05-10 02:46:25 +0530

    amiya

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,15 @@ const About = () => {
   return (
     <section name='about' className='w-full h-screen bg-omio text-gray-300'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-full w-full flex items-center px-8'>
-          <div className=' lg:pb-8 sm:text-right'>
-            <p className='lg:text-3xl font-bold inline border-b-4 border-sky-400 font-omiofont2 sm:text-2.5xl '>
+      <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 px-8'>
+            <p className='lg:text-4xl font-bold inline border-b-4 border-sky-500 text-3xl'>
               About
-            </p> 
+            </p>
           </div>
+          <div></div>
           </div>
-          <div className='max-w-full w-full grid lg:px-8 sm:grid-cols-2 gap-8 px-8  rounded-md  '>
+          <div className='max-w-full w-full grid lg:px-8 sm:grid-cols-2 gap-8 px-8  rounded-md'>
             <div  className='bg-[url(./assets/About.webp)] content-di  rounded-md '>
               
             </div>
