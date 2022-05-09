6c30f20be7180d508efb392563df90cdb8a00c5c
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2022-05-09 23:53:15 +0530

    amiya

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <section name='about' className='w-full h-screen bg-omio text-gray-300'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-full w-full grid grid-cols-2 gap-8 px-8 pb-6'>
+        <div className='max-w-full w-full flex items-center px-8'>
           <div className=' lg:pb-8 sm:text-right'>
             <p className='lg:text-3xl font-bold inline border-b-4 border-sky-400 font-omiofont2 sm:text-2.5xl '>
               About
