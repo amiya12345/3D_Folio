8fdf2f7d81f2f91a75df3093c56f8d3a0dd14e16
Amiya Ranjan sethi <amiyaranjansethi0551@gmail.com>
2022-05-14 12:07:15 +0530

    amiya

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
                         <div class='flex flex-col items-center'>
                             <span className="font-bold text-gray-700 uppercase mb-1 font-omiofont2">Connect with me</span>
                             <div className='text-[24px] flex justify-between items-center py-4 lg:text-[32px]'>
-                                <span className="my-2 px-4"><a href='https://www.behance.net/amiyarsethi1' target='_blank' rel="noreferrer" className="text-nft text-md hover:text-blue-500"><AiOutlineBehance /></a></span>
+                                <span className="my-2 px-4"><a href='https://www.behance.net/amiyarsethi1' target='_blank' rel="noreferrer" className="text-nft text-md hover:text-blue-500 "><AiOutlineBehance /></a></span>
                                 <span className="my-2 px-4"><a href='mailto:amiyaranjansethi0551@gmail.com' target='_blank' rel="noreferrer" className="text-nft text-md hover:text-blue-500"><AiOutlineMail /></a></span>
                                 <span className="my-2 px-4"><a href='https://twitter.com/amiya_ars' target='_blank' rel="noreferrer" className="text-nft text-md hover:text-blue-500"><AiOutlineTwitter /></a></span>
                                 <span className="my-2 px-4"><a href='https://github.com/amiya12345' target='_blank' rel="noreferrer" className="text-nft  text-md hover:text-blue-500"><AiOutlineGithub /></a></span>
