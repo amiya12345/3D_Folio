720ae12fb1ced6cffa03da639648571c8d0a5813
Amiya Ranjan sethi <50417230+amiya12345@users.noreply.github.com>
2022-12-17 19:11:11 +0530

    update bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
       </motion.div>
       <div className="max-h-[100vh] mt-0 mb-auto ml-auto mr-auto overflow-hidden">
         <div className="w-full h-full flex">
-          <Spline scene="https://prod.spline.design/n0Vdaz1MR860yCPb/scene.splinecode" />
+          <Spline scene="https://draft.spline.design/BUvmPL0MpFdDAjqJ/scene.splinecode" />
         </div>
       </div>
     </section>
