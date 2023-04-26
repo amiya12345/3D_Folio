6111516244274a8be29d70c29e06c04fada5d3f6
Amiya Ranjan sethi <50417230+amiya12345@users.noreply.github.com>
2023-04-26 17:00:54 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,7 +98,7 @@ const Navbar = () => {
         </li>
         <li className="py-3 text-2xl">
           <a
-            href="https://drive.google.com/file/d/1ba5ezx06gCnh1rJEasKEqxiaweutHuBR/view?usp=share_link"
+            href="https://drive.google.com/file/d/1QYavr3GQk9gEzY9dxjec9NN2zNrmeLyF/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
