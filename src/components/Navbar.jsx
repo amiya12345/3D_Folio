ef3f82fa32f8ea9bb442b0ad60200771638b104f
Amiya Ranjan sethi <50417230+amiya12345@users.noreply.github.com>
2024-07-01 17:51:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         </li>
         <li className="hover:text-sky-500">
           <a
-            href="https://drive.google.com/file/d/1X_tJFwPZENrybc325suhonCEmZPKcGWq/view"
+            href="https://drive.google.com/file/d/1X_tJFwPZENrybc325suhonCEmZPKcGWq/view?usp=drive_link"
             target="_blank"
             rel="noreferrer"
           >
